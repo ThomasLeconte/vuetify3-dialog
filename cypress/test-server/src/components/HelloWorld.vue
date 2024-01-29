4d7367de7691c9da1f1057c11e9585bbd693c8e5
Thomas Leconte <thomas.leconte@agrial.com>
2024-01-29 14:29:19 +0100

    Fix: clean code

--- cypress/test-server/src/components/HelloWorld.vue
+++ cypress/test-server/src/components/HelloWorld.vue
@@ -7,22 +7,6 @@
 
       <h1 class="text-h2 font-weight-bold">Vuetify</h1>
 
-      <v-btn @click="dialog = true">Open dialog</v-btn>
-
-      <v-dialog
-        v-model="dialog"
-        width="auto"
-      >
-        <v-card>
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="primary" block>Close Dialog</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
       <div class="py-14" />
 
       <div class="d-flex flex-wrap justify-center align-start">
@@ -84,8 +68,7 @@ export default defineComponent({
     MyComponent
   },
   data: () => ({
-    show: true,
-    dialog: false
+    show: true
   }),
   methods: {
     createDialog(){
